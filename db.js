1680e35ce7b0907d6ebc2154b0aa9da8c03ed33d
jotapf2294 <jotapft22@gmail.com>
2026-04-17 02:20:49 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const db = new Dexie("QuintaDB");
-db.version(1).stores({
+db.version(2).stores({ // Aumentamos a versão para 2
     zonas: '++id, nome',
     plantas: '++id, zonaId, tipo, nome, data, ciclo, notas',
-    config: 'key, value'
+    wiki: '++id, titulo, texto' // Nova tabela para a tua Wiki personalizada
 });
